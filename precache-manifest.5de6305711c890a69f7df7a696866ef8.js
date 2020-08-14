7c2480e11faf3df7d442d1c649b898d506d19cd2
howard-yen <howardy2000@gmail.com>
2020-08-14 15:22:12 -0500

    Updates

---
+++ precache-manifest.5de6305711c890a69f7df7a696866ef8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237f4ecad2330797526f481548dc41c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a9f9d66f016282df815",
+    "url": "/static/css/main.a8225c73.chunk.css"
+  },
+  {
+    "revision": "389d2bee582249843d87",
+    "url": "/static/js/2.4cf1a145.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.4cf1a145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9f9d66f016282df815",
+    "url": "/static/js/main.008d01b0.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "ab95adb7c0da395456fe6b45d7dbff90",
+    "url": "/static/media/emailicon.ab95adb7.png"
+  },
+  {
+    "revision": "b028c2e59043116dfc0da6ec6fd26d70",
+    "url": "/static/media/linkedinlogo.b028c2e5.png"
+  }
+]);
