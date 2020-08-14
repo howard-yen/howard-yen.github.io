c904e3f9793fc2a9dcaf7c6ab4a715054b8282be
howard-yen <howardy2000@gmail.com>
2020-08-13 22:15:02 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
 import React from 'react';
-import logo from './logo.svg';
+import BubbleRow from './SocialMediaBubbles'
+
+import ReactPageScroller from "react-page-scroller";
+import Nav from 'react-bootstrap/Nav';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Menu extends React.Component{
+
+
+    render(){
+        return(
+            <Nav defaultActiveKey="0"
+                onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+            className="flex-column">
+                <Nav.Link eventKey="0">About</Nav.Link>
+                <Nav.Link eventKey="1">Project</Nav.Link>
+            </Nav>
+        );
+    };
 }
 
-export default App;
+class Left extends React.Component{
+    render(){
+        return(
+            <div className="Split Left">
+                <header className="Left-header">
+                    <p>
+                        on the left.
+                    </p>
+                </header>
+                <Menu/>
+                <footer>
+                    <BubbleRow />
+                </footer>
+            </div>
+
+        );
+    }
+}
+
+class Block extends React.Component{
+    render(){
+        return(
+            <div className="Block">
+                <header>
+                    <h1>
+                        {this.props.name}
+                    </h1>
+                </header>
+                <body>
+                    <p>
+                        {this.props.content}
+                    </p>
+                </body>
+            </div>
+        );
+    }
+}
+
+class Right extends React.Component{
+    render(){
+        return(
+            <div className="Split Right">
+                <ReactPageScroller
+                    className="Split Right"
+                    pageOnChange={this.props.handlePageChange}
+                    customPageNumber={this.props.currentPage}
+                >
+                    <Block name="about" content="this is about me" />
+                    <Block name="projects" content="this is about my projects"/>
+                </ReactPageScroller>
+            </div>
+        );
+    }
+}
+
+export default class Page extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            currentPage: null,
+        };
+    }
+
+    handlePageChange = pagenum =>{
+        this.setState({currentPage: pagenum});
+    };
+
+    render(){
+        return(
+            <div className="Page">
+                <Left />
+                <Right
+                    pageOnChange={this.handlePageChange}
+                    customPageNumber={this.state.currentPage}
+                />
+            </div>
+        );
+    }
+}
---
+++ src/SocialMediaBubbles.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import githubmark from './images/githubmark.png';
+import linkedinlogo from './images/linkedinlogo.png';
+import emailicon from './images/emailicon.png';
+
+import './SocialMediaBubbles.css';
+
+class LinkBubble extends React.Component{
+    render(){
+        return(
+                <a className="Bubble"
+                    href={this.props.bubble.url}
+                    title={this.props.bubble.name}
+                    target="_blank"
+                    rel="nonreferrer"
+                >
+                    <img
+                        src={this.props.bubble.image}
+                        alt={this.props.bubble.name}
+                        width="25"
+                        height="25"
+                    />
+                </a>
+        );
+    }
+}
+
+export default class BubbleRow extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            bubbles:[
+                {url: "https://github.com/howard-yen/", image: githubmark, name: "github",},
+                {url: "https://www.linkedin.com/in/howard-yen/", image: linkedinlogo, name: "linkedin",},
+                {url: "mailto:howardy2000@gmail.com", image: emailicon, name: "email",}
+            ],
+        };
+    }
+
+    makeBubbles = () =>{
+        const bubbles = [];
+
+        for(let b of this.state.bubbles){
+            bubbles.push(<LinkBubble bubble={b}/>);
+        }
+        return [...bubbles];
+    };
+
+    render(){
+        const bubbles = this.makeBubbles();
+
+        return(
+            <div className="BubbleRow">
+                {bubbles}
+            </div>
+        );
+    }
+}
